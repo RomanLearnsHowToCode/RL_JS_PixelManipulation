a9150f8f30d5ebc77ab43d76dbed55a8599adf6c
Roman <roman.liewehr@hotmail.com>
2022-01-23 18:30:25 +0000

    Particles over image are working

--- script.js
+++ script.js
@@ -1,12 +1,58 @@
-console.log("I am indeed connected");
-
-const canvas = document.getElementById('canvas1');
-const ctx = canvas.getContext('2d');
-canvas.width = 799;
-canvas.height = 950;
-
 const image1 = new Image();
 image1.src = './Assets/backToTheFuture.jpg';
+
+image1.addEventListener('load', function(){
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 799;
+    canvas.height = 950;
+
+    ctx.drawImage(image1, 0,0,canvas.width,canvas.height);
+
+    let particlesArray = [];
+    const numberOfParticles = 10000;
+
+    class Particle {
+        constructor(){
+            this.x = Math.random() * canvas.width;
+            this.y = Math.random();
+            this.speed = 0;
+            this.velocity = Math.random() * 5;
+            this.size = Math.random() * 1.5 + 1;
+        }
+        update(){
+            this.y += this.velocity;
+            if(this.y >= canvas.height){
+                this.y = 0;
+                this.x = Math.random() * canvas.width;
+            }
+        }
+        draw(){
+            ctx.beginPath();
+            ctx.fillStyle = 'white';
+            ctx.arc(this.x, this.y, this.size, 0, Math.PI *2);
+            ctx.fill();
+        }
+    }
+    function init(){
+        for (let i = 0; i < numberOfParticles; i++){
+            particlesArray.push(new Particle);
+        }
+    }
+    init(); // call init to fill the array
+    function animate(){
+        ctx.drawImage(image1, 0,0,canvas.width,canvas.height);
+        ctx.globalAlpha = 0.05;
+        ctx.fillStyle = 'rgb(0,0,0)';
+        ctx.fillRect(0,0,canvas.width, canvas.height);
+        for (let i = 0; i < particlesArray.length; i++){
+            particlesArray[i].update();
+            particlesArray[i].draw();
+        }
+        requestAnimationFrame(animate);
+    }
+    animate();
+});
 
 
 /*
@@ -14,7 +60,7 @@ This function will scan the loaded image,
 then it will count average colour of each pixel, will divide it by 3 and set average colour to each RGB value
 where C1, C2 and C3 are modifing values
 */
-
+/*
 function colourAverage(c1,c2,c3){
     image1.addEventListener('load', function(){ // callback function to wait until the image is loaded
         ctx.drawImage(image1, 0,0);
@@ -34,5 +80,5 @@ function colourAverage(c1,c2,c3){
         scannedImage.data = scannedData;
         ctx.putImageData(scannedImage, 0,0);
     })
-}
-colourAverage(0,0,0);
+}*/
+//colourAverage(0,0,0);
